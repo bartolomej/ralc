290f2b22b7f6a66ad8fcfce12c48079cb49c1fea
Bartolomej Kozorog <bartolomej.kozorog@gmail.com>
2021-11-23 20:59:45 +0100

    init java app

---
+++ src/site/bartolomej/Main.java
@@ -0,0 +1,9 @@
+package site.bartolomej;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // write your code here
+        System.out.println("Welcome to ralc!");
+    }
+}
